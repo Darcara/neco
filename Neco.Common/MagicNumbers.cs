5aa1fcb591b5616819c04685d85ee55c329fa018
Darcara <830228+Darcara@users.noreply.github.com>
2023-10-04 22:50:16 +0200

    Default stream buffer size added

--- Neco.Common/MagicNumbers.cs
+++ Neco.Common/MagicNumbers.cs
@@ -3,7 +3,11 @@ namespace Neco.Common;
 using System;
 
 public static class MagicNumbers {
-	/// Objects larger than or equal to 85.000 bytes are considered a large object and is allocated on the large object heap.
-	/// <value>80 KiB or 81.920 Bytes</value> 
+	/// Objects larger than or equal to 85000 bytes are considered a large object and is allocated on the large object heap.
+	/// <value>80 KiB or 81920 Bytes</value> 
 	public const Int32 MaxNonLohBufferSize = 80 * 1024;
+
+	/// The default Buffer size for many streams and stream operations
+	/// <value>8 KiB or 8192 Bytes</value>
+	public const Int32 DefaultStreamBufferSize = 8192;
 }
