43b5f0cb1be9a1c981d4c526b7ec71b912823ba2
Darcara <830228+Darcara@users.noreply.github.com>
2023-11-10 21:40:35 +0100

    Generic BitHelper

--- Neco.Common/Helper/BitHelper.cs
+++ Neco.Common/Helper/BitHelper.cs
@@ -1,9 +1,67 @@
 namespace Neco.Common.Helper;
 
 using System;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 
 public static class BitHelper {
+
+	/// <summary>
+	/// Check if a bit is set to <see cref="INumberBase{T}.One"/>
+	/// </summary>
+	/// <param name="num">The numeric value</param>
+	/// <param name="bit">The bit to check</param>
+	/// <returns>TRUE if the bit is set, FALSE otherwise</returns>
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	public static Boolean IsBitSet<T>(T num, Int32 bit) where T : INumberBase<T>, IShiftOperators<T, Int32, T>, IBitwiseOperators<T, T, T> {
+		return (num & (T.One << bit)) != T.Zero;
+	}
+
+	/// <summary>
+	/// Set a bit to <see cref="INumberBase{T}.One"/>
+	/// </summary>
+	/// <param name="num">The numeric value</param>
+	/// <param name="bit">The bit to set</param>
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	public static void SetBit<T>(ref T num, Int32 bit) where T : INumberBase<T>, IShiftOperators<T, Int32, T>, IBitwiseOperators<T, T, T> {
+		num |= T.One << bit;
+	}
+
+	/// <summary>
+	/// Clear a bit and set it to <see cref="INumberBase{T}.Zero"/>
+	/// </summary>
+	/// <param name="num">The numeric value</param>
+	/// <param name="bit">The bit to set</param>
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	public static void ClearBit<T>(ref T num, Int32 bit) where T : INumberBase<T>, IShiftOperators<T, Int32, T>, IBitwiseOperators<T, T, T> {
+		num &= ~(T.One << bit);
+	}
+
+	/// <summary>
+	/// Toggles a bit. Sets it to <see cref="INumberBase{T}.One"/> is it was unset, or to <see cref="INumberBase{T}.Zero"/> if it was set.
+	/// </summary>
+	/// <param name="num">The numeric value</param>
+	/// <param name="bit">The bit to toggle</param>
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	public static void ToggleBit<T>(ref T num, Int32 bit) where T : INumberBase<T>, IShiftOperators<T, Int32, T>, IBitwiseOperators<T, T, T> {
+		num ^= T.One << bit;
+	}
+
+	/// <summary>
+	/// Sets or clears a bit.
+	/// </summary>
+	/// <param name="num">The numeric value</param>
+	/// <param name="bit">The bit to set or clear</param>
+	/// <param name="shouldBeSet">TRUE to set the bit, FALSE to clear it</param>
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	public static void Bit<T>(ref T num, Int32 bit, Boolean shouldBeSet) where T : INumberBase<T>, IShiftOperators<T, Int32, T>, IBitwiseOperators<T, T, T> {
+		if (shouldBeSet)
+			num |= T.One << bit;
+		else
+			num &= ~(T.One << bit);
+	}
+
+
 	#region Int16
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -14,7 +72,7 @@ public static class BitHelper {
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void ClearBit(ref Int16 num, Int32 bit) => num &= (Int16)(~(1 << bit));
-	
+
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void ToggleBit(ref Int16 num, Int32 bit) => num ^= (Int16)(1 << bit);
 
@@ -25,9 +83,9 @@ public static class BitHelper {
 		else
 			num &= (Int16)(~(1 << bit));
 	}
-	
+
 	#endregion
-	
+
 	#region UInt16
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -38,7 +96,7 @@ public static class BitHelper {
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void ClearBit(ref UInt16 num, Int32 bit) => num &= (UInt16)(~(1 << bit));
-	
+
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void ToggleBit(ref UInt16 num, Int32 bit) => num ^= (UInt16)(1 << bit);
 
@@ -49,7 +107,6 @@ public static class BitHelper {
 		else
 			num &= (UInt16)(~(1 << bit));
 	}
-	
-	#endregion
 
+	#endregion
 }
--- Neco.Test/Common/Helper/BitHelperTests.cs
+++ Neco.Test/Common/Helper/BitHelperTests.cs
@@ -1,7 +1,9 @@
 namespace Neco.Test.Common.Helper;
 
 using System;
+using System.Numerics;
 using FluentAssertions;
+using Neco.Common.Extensions;
 using Neco.Common.Helper;
 using NUnit.Framework;
 
@@ -29,7 +31,7 @@ public class BitHelperTests {
 		val.Should().Be(64);
 		BitHelper.IsBitSet(val, 7).Should().BeFalse();
 	}
-	
+
 	[Test]
 	public void SetsBitsCorrectlyForUInt16() {
 		UInt16 val = 0;
@@ -51,5 +53,45 @@ public class BitHelperTests {
 		BitHelper.Bit(ref val, 7, false);
 		val.Should().Be(64);
 		BitHelper.IsBitSet(val, 7).Should().BeFalse();
+	}
+
+	[Test]
+	public void SetsBitsCorrectlyForGeneric() {
+		SetsBitsCorrectly<Byte>(4, 32, 64);
+		SetsBitsCorrectly<SByte>(4, 32, 64);
+		SetsBitsCorrectly<Int16>(4, 32, 64);
+		SetsBitsCorrectly<UInt16>(4, 32, 64);
+		SetsBitsCorrectly<Int32>(4, 32, 64);
+		SetsBitsCorrectly<UInt32>(4, 32, 64);
+		SetsBitsCorrectly<Int64>(4, 32, 64);
+		SetsBitsCorrectly<UInt64>(4, 32, 64);
+
+		SetsBitsCorrectly<Int128>(4, 32, 64);
+		SetsBitsCorrectly<UInt128>(4, 32, 64);
+	}
+
+	public void SetsBitsCorrectly<T>(T num4, T num32, T num64) where T : INumberBase<T>, IShiftOperators<T, Int32, T>, IBitwiseOperators<T, T, T> {
+		Console.WriteLine($"Testing {typeof(T).GetName()} with {num4.GetType()}");
+		T val = T.Zero;
+
+		BitHelper.SetBit(ref val, 5);
+		Assert.That(val, Is.EqualTo(num32));
+		val.Should().Be(num32);
+		BitHelper.IsBitSet(val, 5).Should().BeTrue();
+
+		BitHelper.ToggleBit(ref val, 6);
+		val.Should().Be(num64 + num32);
+		BitHelper.IsBitSet(val, 6).Should().BeTrue();
+
+		BitHelper.ClearBit(ref val, 5);
+		val.Should().Be(num64);
+		BitHelper.IsBitSet(val, 5).Should().BeFalse();
+
+		BitHelper.Bit(ref val, 2, true);
+		val.Should().Be(num64 + num4);
+		BitHelper.IsBitSet(val, 2).Should().BeTrue();
+		BitHelper.Bit(ref val, 2, false);
+		val.Should().Be(num64);
+		BitHelper.IsBitSet(val, 2).Should().BeFalse();
 	}
 }
