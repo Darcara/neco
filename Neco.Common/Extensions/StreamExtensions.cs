26465daa3025cf270e9f65f2e6af2c03d503d821
Darcara <830228+Darcara@users.noreply.github.com>
2024-02-02 01:02:39 +0100

    Generalized stream copy from count to inspect

--- Neco.Common/Extensions/StreamExtensions.cs
+++ Neco.Common/Extensions/StreamExtensions.cs
@@ -6,11 +6,14 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
+public delegate void InspectStreamDelegate(Byte[] data, Int32 offset, Int32 length);
+
 public static class StreamExtensions {
 	/// <inheritdoc cref="Stream.CopyTo(Stream, Int32)"/>
 	/// <returns>Number of bytes copied</returns>
-	public static Int64 CopyToAndCount(this Stream source, Stream destination, Int32 bufferSize = MagicNumbers.MaxNonLohBufferSize) {
+	public static Int64 CopyToAndInspect(this Stream source, Stream destination, InspectStreamDelegate inspectCallback, Int32 bufferSize = MagicNumbers.MaxNonLohBufferSize) {
 		ArgumentNullException.ThrowIfNull(destination);
+		ArgumentNullException.ThrowIfNull(inspectCallback);
 		ArgumentOutOfRangeException.ThrowIfNegativeOrZero(bufferSize);
 
 		if (!destination.CanWrite) throw new NotSupportedException("Destination stream not writable");
@@ -23,6 +26,7 @@ public static class StreamExtensions {
 			while ((bytesRead = source.Read(buffer, 0, buffer.Length)) != 0) {
 				destination.Write(buffer, 0, bytesRead);
 				totalBytesCopied += bytesRead;
+				inspectCallback.Invoke(buffer, 0, bytesRead);
 			}
 
 			return totalBytesCopied;
@@ -31,11 +35,12 @@ public static class StreamExtensions {
 			ArrayPool<Byte>.Shared.Return(buffer);
 		}
 	}
-	
+
 	/// <inheritdoc cref="Stream.CopyToAsync(Stream, Int32, CancellationToken)"/>
 	/// <returns>Number of bytes copied</returns>
-	public static async Task<Int64> CopyToAndCountAsync(this Stream source, Stream destination, Int32 bufferSize = MagicNumbers.MaxNonLohBufferSize, CancellationToken cancellationToken = default) {
+	public static async Task<Int64> CopyToAndInspectAsync(this Stream source, Stream destination, InspectStreamDelegate inspectCallback, Int32 bufferSize = MagicNumbers.MaxNonLohBufferSize, CancellationToken cancellationToken = default) {
 		ArgumentNullException.ThrowIfNull(destination);
+		ArgumentNullException.ThrowIfNull(inspectCallback);
 		ArgumentOutOfRangeException.ThrowIfNegativeOrZero(bufferSize);
 
 		if (!destination.CanWrite) throw new NotSupportedException("Destination stream not writable");
@@ -49,6 +54,7 @@ public static class StreamExtensions {
 			while ((bytesRead = await source.ReadAsync(bufferMem, cancellationToken)) != 0) {
 				await destination.WriteAsync(bufferMem.Slice(0, bytesRead), cancellationToken);
 				totalBytesCopied += bytesRead;
+				inspectCallback.Invoke(buffer, 0, bytesRead);
 			}
 
 			return totalBytesCopied;
