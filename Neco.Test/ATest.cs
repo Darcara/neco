38417fb1379542841fb08029b7fc62d99ec3674c
Darcara <830228+Darcara@users.noreply.github.com>
2024-12-10 16:04:02 +0100

    Style fixes and dependencies updated

--- Neco.Test/ATest.cs
+++ Neco.Test/ATest.cs
@@ -4,6 +4,6 @@ using Extensions.Logging.NUnit;
 using Microsoft.Extensions.Logging;
 
 public class ATest {
-	protected static readonly LoggerFactory LoggerFactory = new(new[] { new NUnitLoggerProvider() });
+	protected static readonly LoggerFactory LoggerFactory = new([new NUnitLoggerProvider()]);
 	protected static ILogger<T> GetLogger<T>() => LoggerFactory.CreateLogger<T>();
 }
--- Neco.Test/Common/Processing/BaseContextTests.cs
+++ Neco.Test/Common/Processing/BaseContextTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Neco.Test.Common.Processing;
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using FluentAssertions;
 using Neco.Common.Helper;
@@ -12,33 +13,41 @@ public class BaseContextTests {
 	[Test]
 	public void HandlesFeaturesCorrectly() {
 		BaseContext ctx = new();
-		Dictionary<Object, Object>? features = ReflectionHelper.GetFieldOrPropertyValue<Dictionary<Object, Object>>(ctx, "_features");
+		ConcurrentDictionary<Object, Object>? features = ReflectionHelper.GetFieldOrPropertyValue<ConcurrentDictionary<Object, Object>>(ctx, "_features");
 		features.Should().NotBeNull();
 
 		ctx.SetData("Key", "Data");
-		Assert.That(ctx.GetData<String, String>("Key"), Is.EqualTo("Data"));
-		Assert.That(ctx.GetDataOrDefault<String, String>("Key"), Is.EqualTo("Data"));
-		Assert.That(features, Has.Count.EqualTo(1));
-		Assert.That(features?["Key"], Is.EqualTo("Data"));
-		Assert.That(ctx.TryGetData("Key", out String? availableData), Is.EqualTo(true));
-		Assert.That(availableData, Is.EqualTo("Data"));
-		Assert.That(ctx.GetData("Key"), Is.EqualTo("Data"));
+		Assert.Multiple(() => {
+			Assert.That(ctx.GetData<String, String>("Key"), Is.EqualTo("Data"));
+			Assert.That(ctx.GetDataOrDefault<String, String>("Key"), Is.EqualTo("Data"));
+			Assert.That(ctx.GetDataOrDefault<String, String>("Key", "UnusedDefault"), Is.EqualTo("Data"));
+			Assert.That(features, Has.Count.EqualTo(1));
+			Assert.That(features?["Key"], Is.EqualTo("Data"));
+			Assert.That(ctx.TryGetData("Key", out String? availableData), Is.EqualTo(true));
+			Assert.That(availableData, Is.EqualTo("Data"));
+			Assert.That(ctx.GetData("Key"), Is.EqualTo("Data"));
+		});
 
 		ctx.SetData("KeyAndData");
-		Assert.That(ctx.GetData<Type, String>(typeof(String)), Is.EqualTo("KeyAndData"));
-		Assert.That(ctx.GetDataOrDefault<Type, String>(typeof(String)), Is.EqualTo("KeyAndData"));
-		Assert.That(features, Has.Count.EqualTo(2));
-		Assert.That(features?[typeof(String)], Is.EqualTo("KeyAndData"));
-		Assert.That(ctx.TryGetData(typeof(String), out String? availableKeyAndData), Is.EqualTo(true));
-		Assert.That(availableKeyAndData, Is.EqualTo("KeyAndData"));
+		Assert.Multiple(() => {
+			Assert.That(ctx.GetData<Type, String>(typeof(String)), Is.EqualTo("KeyAndData"));
+			Assert.That(ctx.GetDataOrDefault<Type, String>(typeof(String)), Is.EqualTo("KeyAndData"));
+			Assert.That(features, Has.Count.EqualTo(2));
+			Assert.That(features?[typeof(String)], Is.EqualTo("KeyAndData"));
+			Assert.That(ctx.TryGetData(typeof(String), out String? availableKeyAndData), Is.EqualTo(true));
+			Assert.That(availableKeyAndData, Is.EqualTo("KeyAndData"));
+		});
 
 		ctx.ClearData("KEY");
 		Assert.That(ctx.GetData<String, String>("Key"), Is.EqualTo("Data"));
 
 		ctx.ClearData("Key");
-		Assert.Throws<KeyNotFoundException>(() => ctx.GetData<String, String>("Key"));
-		Assert.That(ctx.GetDataOrDefault<String, String>("Key"), Is.Null);
-		Assert.That(ctx.TryGetData("Key", out String? unavailableData), Is.EqualTo(false));
-		Assert.That(unavailableData, Is.Null);
+		Assert.Multiple(() => {
+			Assert.Throws<KeyNotFoundException>(() => ctx.GetData<String, String>("Key"));
+			Assert.That(ctx.GetDataOrDefault<String, String>("Key"), Is.Null);
+			Assert.That(ctx.GetDataOrDefault<String, String>("Key", "ProvidedDefault"), Is.EqualTo("ProvidedDefault"));
+			Assert.That(ctx.TryGetData("Key", out String? unavailableData), Is.EqualTo(false));
+			Assert.That(unavailableData, Is.Null);
+		});
 	}
 }
