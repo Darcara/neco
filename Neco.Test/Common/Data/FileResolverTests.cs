b2dcfc27551cb37342a22b2fc632de3edcbb9d2a
Darcara <830228+Darcara@users.noreply.github.com>
2025-10-06 00:14:16 +0200

    Added missing tests

---
+++ Neco.Test/Common/Data/FileResolverTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Neco.Test.Common.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Neco.Common.Data;
+using NUnit.Framework;
+
+[TestFixture]
+public class FileResolverTests {
+	[Test]
+	public void EnumeratesSearchDirectories() {
+		List<String> directories = FileResolver.WindowsFileSearchLocations(false).ToList();
+		Console.WriteLine($"Search directories:{Environment.NewLine}{String.Join(Environment.NewLine, directories)}");
+		directories.Should().Contain(Environment.CurrentDirectory);
+	}
+	
+	[Test]
+	public void CanResolve() {
+		String filename = typeof(FileResolverTests).Assembly.GetName().Name + ".dll";
+		List<FileInfo> fileInfos = new FileResolver(FileResolver.WindowsFileSearchLocations(false)).Resolve(filename).ToList();
+		fileInfos.Should().HaveCount(1);
+	}
+}
---
+++ Neco.Test/Common/Data/SequentialGuidGeneratorTests.cs
@@ -0,0 +1,63 @@
+namespace Neco.Test.Common.Data;
+
+using System;
+using System.Threading;
+using Neco.Common.Data;
+using Neco.Common.Extensions;
+using Neco.Common.Helper;
+using NUnit.Framework;
+
+[TestFixture]
+public class SequentialGuidGeneratorTests {
+	[SetUp]
+	public void BeforeTest() {
+		// WarmUp JIT
+		for (Int32 i = 0; i < 100; i++) SequentialGuidGenerator.CreateSequentialGuid();
+
+		// Wait for clock to advance after current id tick
+		Thread.Sleep(10);
+	}
+
+	[Test]
+	public void GeneratesSequentialGuids() {
+		Guid g1 = SequentialGuidGenerator.CreateSequentialGuid();
+		Thread.Sleep(1);
+		Guid g2 = SequentialGuidGenerator.CreateSequentialGuid();
+		
+		Console.WriteLine($"{g1} -- {g1.ToByteArray().ToStringHexSingleLine(0,4)}-{g1.ToByteArray().ToStringHexSingleLine(4,2)}-{g1.ToByteArray().ToStringHexSingleLine(6)}");
+		Console.WriteLine($"{g2} -- {g2.ToByteArray().ToStringHexSingleLine(0,4)}-{g2.ToByteArray().ToStringHexSingleLine(4,2)}-{g2.ToByteArray().ToStringHexSingleLine(6)}");
+		Assert.That(g2, Is.GreaterThan(g1));
+	}
+	
+	[Test]
+	public void CanEstimateTimeFromId() {
+		DateTime now = DateTime.UtcNow;
+		Guid g1 = SequentialGuidGenerator.CreateSequentialGuid();
+		DateTime idTime = SequentialGuidGenerator.FromSequentialGuid(g1);
+		Console.WriteLine(now.ToString("O"));
+		Console.WriteLine(idTime.ToString("O"));
+		Assert.That(idTime.Kind, Is.EqualTo(DateTimeKind.Utc));
+		Assert.That(idTime - now, Is.GreaterThan(TimeSpan.FromMicroseconds(-20)));
+		Assert.That(idTime - now, Is.InRange(TimeSpan.FromMicroseconds(-20), TimeSpan.FromMicroseconds(20)));
+	}
+	
+	// AMD Ryzen 9 3900X 12-Core Processor
+	// CreateSequentialBinary 43,056,827 ops in 5,000.001ms = clean per operation: 0.084µs or 11,888,914.112op/s with GC 0/0/0
+	// CreateSequentialBinary TotalCPUTime per operation: 5,000.000ms or clean 11,888,915.753op/s for a factor of 1.000
+	[Test]
+	[Category("Benchmark")]
+	public void RoughGenerationBenchmark() {
+		PerformanceHelper.GetPerformanceRough(nameof(SequentialGuidGenerator.CreateSequentialGuid), () => SequentialGuidGenerator.CreateSequentialGuid());
+	}
+	
+	[Test]
+	public void ConvertsToMySqlUuidCorrectly() {
+		Guid newGuid = Guid.NewGuid();
+		Guid convertedGuid = SequentialGuidGenerator.FromMySqlUuid(SequentialGuidGenerator.ToMySqlUuid(newGuid));
+		Assert.That(convertedGuid, Is.EqualTo(newGuid));
+
+		Byte[] array = newGuid.ToByteArray();
+		Byte[] convertedArray = SequentialGuidGenerator.ToMySqlUuid(SequentialGuidGenerator.FromMySqlUuid(array));
+		Assert.That(convertedArray, Is.EqualTo(array));
+	}
+}
